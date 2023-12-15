6b7b36b55d337d29711346ad185bd99dac169157
tsang <51614765+quangsang52sh@users.noreply.github.com>
2023-12-15 13:04:31 +0700

    Update Quyen_MetaPipeline.sh

--- data/Quyen_MetaPipeline.sh
+++ data/Quyen_MetaPipeline.sh
@@ -17,7 +17,9 @@ copepod_output="copepod_output"
 
 # Step 1: Remove adapters with Trimmomatic
 echo "Step 1: Removing adapters with Trimmomatic..."
-trimmomatic SE -phred33 "$input_fastq" "$output_trimmed" ILLUMINACLIP:"$adapters_file":2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 2> "$log_file"
+trimmomatic SE -phred33 "$input_fastq" "$output_trimmed" \
+    ILLUMINACLIP:"$adapters_file":2:30:10 \
+    LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 2> "$log_file"
 
 # Step 2: Quality control
 echo "Step 2: Running FastQC for quality control..."
@@ -30,15 +32,20 @@ Rscript dada2_script.R "$output_trimmed" "$dada2_output"
 
 # Step 4: Remove adapters and low-quality reads from shotgun metagenomics using Kneaddata
 echo "Step 4: Removing adapters and low-quality reads with Kneaddata..."
-kneaddata --input "$input_fastq" --output "$kneaddata_output" --reference-db kneaddata_reference_db
+kneaddata --input "$input_fastq" --output "$kneaddata_output" \
+    --reference-db kneaddata_reference_db
 
 # Step 5: Analyze OTUs with Qiime2 (for 16S rRNA data)
 echo "Step 5: Analyzing OTUs with Qiime2..."
-qiime dada2 denoise-single --i-demultiplexed-seqs "$output_trimmed" --p-trim-left 0 --p-trunc-len 0 --o-table "$qiime2_output/table.qza" --o-representative-sequences "$qiime2_output/rep-seqs.qza"
+qiime dada2 denoise-single --i-demultiplexed-seqs "$output_trimmed" \
+    --p-trim-left 0 --p-trunc-len 0 \
+    --o-table "$qiime2_output/table.qza" \
+    --o-representative-sequences "$qiime2_output/rep-seqs.qza"
 
 # Step 6: Analyze metagenomics with Metaphlan 3.0
 echo "Step 6: Analyzing metagenomics with Metaphlan 3.0..."
-metaphlan "$kneaddata_output/filtered.fastq" --input_type fastq --output_file "$metaphlan_output"
+metaphlan "$kneaddata_output/filtered.fastq" --input_type fastq \
+    --output_file "$metaphlan_output"
 
 # Step 7: Create Venn diagrams using R's "venn" package
 echo "Step 7: Creating Venn diagrams..."
@@ -51,13 +58,16 @@ Rscript phyloseq_analysis.R "$qiime2_output/table.qza" "$phyloseq_output"
 # Step 9: Prepare reference database and use blastn
 echo "Step 9: Preparing reference database and using blastn..."
 makeblastdb -in reference_sequences.fasta -dbtype nucl -out "$blastn_db"
-blastn -query query_sequence.fasta -db "$blastn_db" -out blastn_results.txt -outfmt "6 qseqid sseqid pident length evalue"
+blastn -query query_sequence.fasta -db "$blastn_db" \
+    -out blastn_results.txt -outfmt "6 qseqid sseqid pident length evalue"
 
 # Step 10: Analyze diversity of gut copepod microbiota with micom
 echo "Step 10: Analyzing diversity of gut copepod microbiota with micom..."
 # Ensure you have micom installed and the necessary databases set up
 # Replace placeholders with actual paths and filenames
-micom community --table $copepod_output/otu_table.txt --metadata $copepod_output/metadata.txt --out_dir $copepod_output/micom_output
+micom community --table $copepod_output/otu_table.txt \
+    --metadata $copepod_output/metadata.txt \
+    --out_dir $copepod_output/micom_output
 
 # Step 11: Generate a final summary report
 echo "Step 11: Generating a final summary report..."
